611a5c4a9c32d1a1244405ea83a65d05ba3896b4
ColumMaher <colummaher1@gmail.com>
2023-02-22 12:19:02 +0000

    Revert "Merge branch 'main' of https://github.com/ColumMaher/CS4227-Assigment1"
    This reverts commit eb53b5bc5c13407e63c4b627d7051f53fcfb71bc, reversing
    changes made to c3d1918b0a30034862d260cc432ba72fabdfe2aa.

--- src/Logger.java
+++ src/Logger.java
@@ -13,19 +13,9 @@ public class Logger implements Interceptor{
     public void setContext(Context c) {
         this.context = c;
     }
-    @Override
-    public void interceptFrequentRenterPoints(Context c) {
-        Customer customer = c.getCustomer();
-        int currentRenterPoints = 0;
-        for (Rental rental: customer.getRentals()){
-            currentRenterPoints += rental.getFrequentRenterPoints();
-            System.out.println("LOG: Points for rental " + rental.getMovie().getTitle() + " " + currentRenterPoints);
-        }
-        System.out.println("LOG: Total current rental points: " + currentRenterPoints);
-    }
 
     @Override
-    public void interceptGetCharge(Context c) {
+    public void log(Context c) {
 
     }
 }
